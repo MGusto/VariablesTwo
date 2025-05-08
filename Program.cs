5b489bf765b5651fff3b8a86f62a001977e0dcde
MGusto <felixbjorseth@outlook.com>
2025-05-08 11:20:36 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace VariablesTwo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
